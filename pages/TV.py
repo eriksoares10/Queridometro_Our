fb21a98703d1592dac27c1ee9a0e7cb03ae78dab
eriksoares10 <eriksilsoares@gmail.com>
2026-09-23 23:56:36 -0300

    Refactor emoji HTML generation in TV.py

--- pages/TV.py
+++ pages/TV.py
@@ -224,19 +224,7 @@ while True:
 
         for emoji, quantidade in contagem.items():
 
-            emojis_html += f"""
-            <div class="emoji-item">
-
-                <div class="emoji">
-                    {emoji}
-                </div>
-
-                <div class="quantidade">
-                    {quantidade}
-                </div>
-
-            </div>
-            """
+            emojis_html += f"""<div class="emoji-item"><div class="emoji">{emoji}</div><div class="quantidade">{quantidade}</div></div>"""
 
         # ----------------------------------------------------
         # CASO NÃO TENHA VOTOS
@@ -254,26 +242,15 @@ while True:
         # MOSTRA O PARTICIPANTE
         # ----------------------------------------------------
 
-        tela.markdown(
-            f"""
-            <div class="painel">
-
-                <div class="nome">
-                    {pessoa}
-                </div>
-
-                <div class="linha-emojis">
-                    {emojis_html}
-                </div>
-
-                <div class="rodape">
-                    QUERIDÔMETRO DA OUR
-                </div>
-
-            </div>
-            """,
-            unsafe_allow_html=True
-        )
+       
+         tela.markdown(
+    f"""<div class="painel">
+<div class="nome">{pessoa}</div>
+<div class="linha-emojis">{emojis_html}</div>
+<div class="rodape">QUERIDÔMETRO DA OUR</div>
+</div>""",
+    unsafe_allow_html=True
+)
 
         # Tempo de exibição de cada participante
         time.sleep(5)
